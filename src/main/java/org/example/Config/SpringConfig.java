2af78b8d425455448001cc21eb4fb2a1ede6db47
JavohirXatamov10 <xatamovjavohir70@gmail.com>
2024-05-10 15:32:36 +0500

    spring mvc va jdbc kutubxona qo`shildi.mvc.Configuratsiyalari boldi index.htmlda hello yozuv chiqdi

---
+++ src/main/java/org/example/Config/SpringConfig.java
@@ -0,0 +1,25 @@
+package org.example.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("org.example")
+public class SpringConfig {
+    @Bean
+    public DriverManagerDataSource dataSource(){
+        var dataSources=new DriverManagerDataSource("jdbc:postgresql://localhost:5432/book8mod4home",
+        "postgres", "root123");
+        dataSources.setDriverClassName("org.postgresql.Driver");
+        return dataSources;
+    }
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(dataSource());
+    }
+
+}
